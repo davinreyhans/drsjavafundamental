0c7e358cea79ee7f0bb0a01b551fc44c4f895778
davinreyhans <[daavinreyhans@gmail.com]>
2023-06-28 17:10:33 +0700

    added Praktikum2, implementing exception handling try-catch-finally

---
+++ src/main/java/com/davin/drsjavafundamental/testharian/praktikum2/Praktikum2.java
@@ -0,0 +1,37 @@
+package com.davin.drsjavafundamental.testharian.praktikum2;
+/*
+IntelliJ IDEA 2022.3.1 (Ultimate Edition)
+Build #IU-223.8214.52, built on December 20, 2022
+@Author Davin Reyhan S a.k.a. Davin Reyhan Setiawan
+Java Developer
+Created on 6/28/2023 4:07 PM
+@Last Modified 6/28/2023 4:07 PM
+Version 1.0
+*/
+
+import java.util.Scanner;
+
+public class Praktikum2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int intInput;
+        double doJumlah = 0;
+        double doCounter = 0;
+
+        System.out.println("Input: ");
+        try {
+            do{
+                intInput = scanner.nextInt();
+                doJumlah += intInput;
+                doCounter++;
+            }while (true);
+        } catch (Exception e) {
+            double doAverage = doJumlah/doCounter;
+            System.out.println("\nRata-rata: " + doAverage);
+        } finally {
+            System.out.println("\nDone.");
+        }
+    }
+}
+
+
